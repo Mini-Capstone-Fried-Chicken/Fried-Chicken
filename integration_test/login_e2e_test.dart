2ef730dc664741a9c5f3db68b9eb7e7ac0ecf462
Yuioytffhio <hiba.tal05@gmail.com>
2026-02-08 13:35:43 -0500

    Added empty field, wrong field and spec char test cases in login e2e, #62

--- integration_test/login_e2e_test.dart
+++ integration_test/login_e2e_test.dart
@@ -8,7 +8,65 @@ import 'package:campus_app/features/explore/ui/explore_screen.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  //verify that the user can't login with wrong credentials and that the appropriate error message is shown
+  testWidgets('Displays error for invalid credentials', (tester) async {
+    app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 5));
 
+    final emailField = find.byKey(const Key('login_email'));
+    final passwordField = find.byKey(const Key('login_password'));
+    final loginButton = find.byKey(const Key('login_button'));
+
+    await tester.enterText(emailField, 'wrong.email@example.com');
+    await tester.enterText(passwordField, 'wrongpassword');
+    await tester.pumpAndSettle();
+    await tester.ensureVisible(loginButton);
+
+    // Tap login button
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+    expect(find.text('Invalid email or password.'), findsOneWidget);
+  });
+
+  // verify that the user can't login with empty fields and that the appropriate error message is shown
+  testWidgets('Displays error for empty fields', (tester) async {
+    app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    final loginButton = find.byKey(const Key('login_button'));
+
+    await tester.pumpAndSettle();
+    await tester.ensureVisible(loginButton);
+
+    // Tap login button
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+
+    expect(find.text('Please enter a valid email address.'), findsOneWidget);
+  });
+
+  // verify that the user can't login with special characters in email and password and that the appropriate error message is shown
+  testWidgets('Handles special characters in email and password', (tester) async {
+    app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    final emailField = find.byKey(const Key('login_email'));
+    final passwordField = find.byKey(const Key('login_password'));
+    final loginButton = find.byKey(const Key('login_button'));
+
+    await tester.enterText(emailField, '!@#\$%^&*()');
+    await tester.enterText(passwordField, '!@#\$%^&*()');
+    await tester.pumpAndSettle();
+    await tester.ensureVisible(loginButton);
+
+    // Tap login button
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+
+    expect(find.text('Invalid email or password.'), findsOneWidget);
+  });
+
+  // verify that the user can log in and reach the dashboard with valid credentials 
   testWidgets('User can log in and reach dashboard', (tester) async {
 
     //runApp(const MaterialApp(home: SignInPage()));
