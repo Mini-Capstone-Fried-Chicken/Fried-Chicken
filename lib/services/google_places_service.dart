f3172be7df157b3a997f0f8088848b69a3af021f
HaichuanLi <lihaichuan2011@hotmail.com>
2026-02-15 23:58:42 -0500

    linked to correct api project
    Can now search non concordia buildings. Restricted the number of concordia search results to 2

--- lib/services/google_places_service.dart
+++ lib/services/google_places_service.dart
@@ -45,7 +45,7 @@ class PlaceResult {
 
 /// Service for searching places using Google Maps Places API (New)
 class GooglePlacesService {
-  static const String _apiKey = 'AIzaSyCGyz5Z6uLqqBDVBJnVUeZ4l23fJ6iXE6s';
+  static const String _apiKey = 'AIzaSyBu67d8z_Vd2eUsfASieUTgwkDHlRmk8GY';
   static const String _baseUrl = 'https://places.googleapis.com/v1';
 
   /// Search for places using Google Places API Text Search
